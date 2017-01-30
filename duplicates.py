822aa13fcd52afeaf2c33a4e40f52b339933b31b
semenchenkovital <semenchenkovital@gmail.com>
2017-01-30 14:56:30 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,13 @@
-
+import os
+def walk(dir,a):
+    for name in os.listdir(dir):
+    path = os.path.join(dir, name)
+    if os.path.isfile(path):
+        print(path)
+        
+    else:
+        walk(path)
+file = open("d:\\d.josm", "w")
+walk('d:\\BGS')
 if __name__ == '__main__':
     pass
